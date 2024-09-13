0a5b75967e6d7d5b2636532e332ed82c7d583911
Joyriver-laptop <dlrkd1122@gmail.com>
2024-09-13 11:23:59 +0900

    프언 hw01 끝

--- 프로그래밍언어 - 박지혁/hw/scala-tutorial/src/main/scala/kuplrg/Implementation.scala
+++ 프로그래밍언어 - 박지혁/hw/scala-tutorial/src/main/scala/kuplrg/Implementation.scala
@@ -50,43 +50,133 @@ object Implementation extends Template {
     }
   }
 
-  def generate(init: Int, f: Int => Int, n: Int): List[Int] = ???
+  def generate(init: Int, f: Int => Int, n: Int): List[Int] = {
+    if n == 0 then Nil else {
+      init :: generate(f(init), f, n - 1)
+    }
+  }
 
-  def incKey(map: Map[String, Int], key: String): Map[String, Int] = ???
+  def incKey(map: Map[String, Int], key: String): Map[String, Int] = {
+    val x = map.get(key)
+    if x == None then map else {
+      map + (key -> (x.getOrElse(0) + 1)) 
+    }
+  }
 
   def validSums(
     l: List[Int],
     r: List[Int],
     f: (Int, Int) => Boolean,
-  ): Set[Int] = ???
+  ): Set[Int] = {
+
+    l.flatMap (x => 
+      r.filter (y => f(x, y))
+       .map(y => x + y)
+    ).toSet
+  }
 
   // ---------------------------------------------------------------------------
   // Trees
   // ---------------------------------------------------------------------------
   import Tree.*
 
-  def count(t: Tree, x: Int): Int = ???
+  def count(t: Tree, x: Int): Int = {
+    t match {
+      case Leaf(n) => if n == x then 1 else 0
+      case Branch(l, n, r) => if n == x then 1 + count(l, x) + count(r, x) else count(l, x) + count(r, x)
+    }
+  }
 
-  def heightOf(t: Tree): Int = ???
+  def heightOf(t: Tree): Int = {
+    t match {
+      case Leaf(n) => 0
+      case Branch(l, n, r) => {
+        val hl = heightOf(l) + 1
+        val hr = heightOf(r) + 1
+        if hl > hr then hl else hr
+      }
+    }
+  }
 
-  def min(t: Tree): Int = ???
+  def min(t: Tree): Int = {
+    t match {
+      case Leaf(n) => n
+      case Branch(l, n, r) => {
+        val ml = min(l)
+        val mr = min(r)
+        if n <= ml && n <= mr then n else
+          if ml <= n && ml <= mr then ml else mr
+      }
+    }
+  }
 
-  def sumLeaves(t: Tree): Int = ???
+  def sumLeaves(t: Tree): Int = {
+    t match {
+      case Leaf(n) => n
+      case Branch(l, n , r) => {
+        sumLeaves(l) + sumLeaves(r)
+      }
+    }
+  }
 
-  def inorder(t: Tree): List[Int] = ???
+  def inorder(t: Tree): List[Int] = {
+    t match {
+      case Leaf(n) => List(n)
+      case Branch(l, n, r) => inorder(l) ::: List(n) ::: inorder(r)
+    }
+  }
 
   // ---------------------------------------------------------------------------
   // Boolean Expressions
   // ---------------------------------------------------------------------------
   import BE.*
 
-  def isLiteral(expr: BE): Boolean = ???
+  def isLiteral(expr: BE): Boolean = {
+    expr match {
+      case Literal(b) => true 
+      case _ => false
+    }
+  }
 
-  def countImply(expr: BE): Int = ???
+  def countImply(expr: BE): Int = {
+    expr match {
+      case Imply(l, r) => 1 + countImply(l) + countImply(r)
+      case And(l, r) => countImply(l) + countImply(r)
+      case Or(l, r) => countImply(l) + countImply(r)
+      case Not(b) => countImply(b)
+      case _ => 0
+    }
+  }
 
-  def literals(expr: BE): List[Boolean] = ???
+  def literals(expr: BE): List[Boolean] ={
+    expr match {
+      case Literal(b) => List(b)
+      case Imply(l, r) => literals(l) ::: literals(r)
+      case And(l, r) => literals(l) ::: literals(r)
+      case Or(l, r) => literals(l) ::: literals(r)
+      case Not(b) => literals(b)
+      case _ => Nil
+    }
+  }
 
-  def getString(expr: BE): String = ???
+  def getString(expr: BE): String = {
+    expr match {
+      case Literal(b) => if b then "#t" else "#f"
+      case Imply(l, r) => "(" + getString(l) + " => " + getString(r) + ")"
+      case And(l, r) => "(" + getString(l) + " & " + getString(r) + ")"
+      case Or(l, r) => "(" + getString(l) + " | " + getString(r) + ")"
+      case Not(b) => "!" + getString(b)
+      case _ => ""
+    }
+  } 
 
-  def eval(expr: BE): Boolean = ???
+  def eval(expr: BE): Boolean = {
+    expr match {
+      case Literal(b) => if b then true else false
+      case Not(b) => if eval(b) then false else true
+      case And(l, r) => if eval(l) && eval(r) then true else false
+      case Or(l, r) => if eval(l) || eval(r) then true else false
+      case Imply(l, r) => if !eval(l) || eval(r) then true else false
+    }
+  }
 }
