8f1735da7de924329239ebe2dfc09f052b80bb24
Joyriver-laptop <dlrkd1122@gmail.com>
2024-11-14 10:23:28 +0900

    업데이트

--- 프로그래밍언어 - 박지혁/hw/kfae/src/main/scala/kuplrg/Implementation.scala
+++ 프로그래밍언어 - 박지혁/hw/kfae/src/main/scala/kuplrg/Implementation.scala
@@ -6,5 +6,21 @@ object Implementation extends Template {
   import Value.*
   import Cont.*
 
-  def reduce(k: Cont, s: Stack): (Cont, Stack) = ???
+  def reduce(k: Cont, s: Stack): (Cont, Stack) = k match{
+    case EmptyK => (k, s)
+    case EvalK(e, env, k) => e match {
+      case Num(n) => (k, NumV(n) :: s)
+      case Add(l, r) => (EvalK(l, env, EvalK(r, env, AddK(k))), s)
+      case Mul(l, r) => (EvalK(l, env, EvalK(r, env, MulK(k))), s)
+      case Id(id) => (k, env.getOrElse(id, error("free identifier")) :: s)
+      case Fun(p, b) => (k, CloV(p, b, env) :: s)
+      case App(f, e) => (EvalK(f, env, EvalK(e, env, AppK(k))), s)
+      case Vcc(x, b) => (EvalK(b, env + (x -> ContV(k, s)), s), s)
+    }
+    case AddK(k) => {
+      s match {
+        case NumV(rn) :: Num
+      }
+    }
+  }
 }
