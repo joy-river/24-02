cae124b2b431733145d98851497125adfb7e0b6d
Joyriver-laptop <dlrkd1122@gmail.com>
2024-11-14 12:43:00 +0900

    완료

--- 프로그래밍언어 - 박지혁/hw/kfae/src/main/scala/kuplrg/Implementation.scala
+++ 프로그래밍언어 - 박지혁/hw/kfae/src/main/scala/kuplrg/Implementation.scala
@@ -8,18 +8,32 @@ object Implementation extends Template {
 
   def reduce(k: Cont, s: Stack): (Cont, Stack) = k match{
     case EmptyK => (k, s)
-    case EvalK(e, env, k) => e match {
+    case EvalK(env, e, k) => e match {
       case Num(n) => (k, NumV(n) :: s)
-      case Add(l, r) => (EvalK(l, env, EvalK(r, env, AddK(k))), s)
-      case Mul(l, r) => (EvalK(l, env, EvalK(r, env, MulK(k))), s)
+      case Add(l, r) => (EvalK(env, l, EvalK(env, r, AddK(k))), s)
+      case Mul(l, r) => (EvalK(env, l, EvalK(env, r, MulK(k))), s)
       case Id(id) => (k, env.getOrElse(id, error("free identifier")) :: s)
       case Fun(p, b) => (k, CloV(p, b, env) :: s)
-      case App(f, e) => (EvalK(f, env, EvalK(e, env, AppK(k))), s)
-      case Vcc(x, b) => (EvalK(b, env + (x -> ContV(k, s)), s), s)
+      case App(f, e) => (EvalK(env, f, EvalK(env, e, AppK(k))), s)
+      case Vcc(x, b) => (EvalK(env + (x -> ContV(k, s)), b, k), s)
     }
     case AddK(k) => {
       s match {
-        case NumV(rn) :: Num
+        case NumV(rn) :: NumV(ln) :: s => (k, NumV(rn + ln) :: s)
+        case _ => error("invalid operation")
+      }
+    }
+    case MulK(k) => {
+      s match {
+        case NumV(rn) :: NumV(ln) :: s => (k, NumV(rn * ln) :: s)
+        case _ => error("invalid operation")
+      }
+    }
+    case AppK(k) => {
+      s match {
+        case v2 :: CloV(p, b, env) :: s => (EvalK(env + (p -> v2), b, k), s)
+        case v2 :: ContV(k2, s2) :: s => (k2, v2 :: s2)
+        case _ => error("not a function")
       }
     }
   }
