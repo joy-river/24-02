1acecf66ea9f7edd44032c14f7360a758d437b46
Joyriver-laptop <dlrkd1122@gmail.com>
2024-12-02 12:19:47 +0900

    업데이트

--- 프로그래밍언어 - 박지혁/hw/atfae/src/main/scala/kuplrg/Implementation.scala
+++ 프로그래밍언어 - 박지혁/hw/atfae/src/main/scala/kuplrg/Implementation.scala
@@ -105,7 +105,27 @@ object Implementation extends Template {
       }
     }
     case TypeDef(t, varts, e) => {
-      
+      typeCheck(Id(t), tenv)
+      val newtEnv = tenv.addType(t, varts.map(vart => vart.name -> vart.ptys).toMap)
+      if (!varts.forall(vart => vart.ptys.forall(pty => wellFormed(pty, newtEnv))))
+      then error("Type error")
+      varts.foreach( vart =>
+        newtEnv.addVar(vart.name, ArrowT(vart.ptys, NameT(t)))
+      )
+      val rty = typeCheck(e, newtEnv)
+      if (!wellFormed(rty, tenv)) then error("Type error")
+      rty
+    }
+
+    case Match(e, mcases) => typeCheck(e, tenv) match{
+      case NameT(name) => {
+        val tmap = tenv.tys.getOrElse(name, error("Type error"))
+        mustValidMatch(mcases, tmap)
+        val tys = for (MatchCase(x, ps, b) <- mcases)
+          yield typeCheck(b, tenv.addVars((ps zip tmap(x))))
+        tys.reduce((lty, rty) => { mustSame(lty, rty); lty })
+      }
+      case _ => error("Type error")
     }
 
   }
@@ -117,5 +137,124 @@ object Implementation extends Template {
       case None => error("undefined variable")
     }
   }
-  def interp(expr: Expr, env: Env): Value = ???
+
+  def mustValidMatch(cs: List[MatchCase], tmap: Map[String, List[Type]]): Unit =
+    val xs = cs.map(_.name).toSet
+    if (xs.size != cs.size) error("invalid match") // duplicate cases
+    if (tmap.keySet != xs) error("invalid match") // non-exhaustive cases
+    for (MatchCase(x, ps, _) <- cs if (tmap(x).size != ps.size))
+      error("invalid match") 
+
+  def mustSame(lty: Type, rty: Type): Unit = lty match {
+    case NumT | BoolT => if (lty != rty) error("Type error")
+    case ArrowT(ltps, lrty) => rty match {
+      case ArrowT(rtps, rrty) => {
+        if (ltps.size != rtps.size) error("Type error")
+        for ((ltp, rtp) <- (ltps zip rtps)) mustSame(ltp, rtp)
+        mustSame(lrty, rrty)
+      }
+      case _ => error("Type error")
+    }
+    case NameT(lname) => rty match {
+      case NameT(rname) => if (lname != rname) error("Type error")
+      case _ => error("Type error")
+    }
+  }
+
+
+  def interp(expr: Expr, env: Env): Value = expr match {
+    case Num(n) => NumV(n)
+    case Bool(b) => BoolV(b)
+    case Add(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => NumV(n1 + n2)
+        case _ => error("invalid operation")
+      }
+    }
+    case Mul(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => NumV(n1 * n2)
+        case _ => error("invalid operation")
+      }
+    }
+    case Div(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => if (n2 == 0) error("invalid operation") else NumV(n1 / n2)
+        case _ => error("invalid operation")
+      }
+    }
+    case Mod(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => if (n2 == 0) error("invalid operation") else NumV(n1 % n2)
+        case _ => error("invalid operation")
+      }
+    }
+    case Eq(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => BoolV(n1 == n2)
+        case _ => error("invalid operation")
+      }
+    }
+    case Lt(e1, e2) => {
+      val v1 = interp(e1, env)
+      val v2 = interp(e2, env)
+      (v1, v2) match {
+        case (NumV(n1), NumV(n2)) => BoolV(n1 < n2)
+        case _ => error("invalid operation")
+      }
+    }
+    
+    case Val(x, e1, e2) => {
+      val v1 = interp(e1, env)
+      interp(e2, env + (x -> v1))
+    }
+
+    case Id(x) => env.get(x) match {
+      case Some(v) => v
+      case None => error("undefined variable")
+    }
+
+    case Fun(p, b) => CloV(p.map(p => p.name), b, () => env)
+
+    case Rec(x, params, rty, e0, e1) => {
+      lazy val newEnv : Env = env + (x -> CloV(params.map(p => p.name), e0, () => newEnv))
+      interp(e1, newEnv)
+    }
+
+    case App(f, args) => {
+      val argv = args.map(arg => interp(arg, env))
+      interp(f, env) match {
+        case CloV(params, b, e) => {
+          interp(b, e() ++ params.zip(argv))
+        }
+        case ConstrV(name) => VariantV(name, argv)
+        case _ => error("not a function or constructor")
+      }
+    }
+
+    case If(e0, e1, e2) => interp(e0, env) match {
+      case BoolV(true) => interp(e1, env)
+      case BoolV(false) => interp(e2, env)
+      case _ => error("invalid condition")
+    }
+
+    case TypeDef(t, varts, e) => {
+      interp(e, env ++ varts.map(vart => vart.name -> ConstrV(vart.name)))
+    }
+
+    
+
+
+
+  }
 }
